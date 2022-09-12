1ef994361b7eab299b2d647c298d45949472a167
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2022-09-12 15:59:03 +0200

    Rydd i klientkode (#3)
    * Rydd i klientkode
    
    * Fjern wildcard-unntak for linter

--- src/test/kotlin/no/nav/helsearbeidsgiver/altinn/AltinnClientTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/altinn/AltinnClientTest.kt
@@ -1,28 +1,95 @@
 package no.nav.helsearbeidsgiver.altinn
 
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.row
 import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.shouldBe
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
 import io.ktor.client.plugins.ServerResponseException
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.serialization.jackson.jackson
+
+private const val IDENTITETSNUMMER = "01020354321"
+
+private val validAltinnResponse = "organisasjoner-med-rettighet.json".readResource()
 
 class AltinnClientTest : StringSpec({
+    "id har kun rettigheter tilknyttet organisasjoner som Altinn returnerer og som er underenheter" {
+        val altinnClient = mockAltinnClient(HttpStatusCode.OK, validAltinnResponse)
 
-    "valid answer from altinn returns properly serialized list of all active org forms" {
-        val authList = buildClient(HttpStatusCode.OK, validAltinnResponse).hentOrgMedRettigheterForPerson(identitetsnummer)
+        listOf(
+            row("910098896", true),
+            row("910020102", false), // Mangler hovedenhet
+            row("123456789", false) // Er ikke i listen fra responsen
+        )
+            .forEach { (orgnr, expected) ->
+                altinnClient.harRettighetForOrganisasjon(IDENTITETSNUMMER, orgnr)
+                    .shouldBe(expected)
+            }
+    }
+
+    "gyldig svar fra Altinn gir liste av organisasjoner" {
+        val altinnClient = mockAltinnClient(HttpStatusCode.OK, validAltinnResponse)
+
+        val authList = altinnClient.hentRettighetOrganisasjoner(IDENTITETSNUMMER)
 
         authList.size shouldBeExactly 4
     }
 
-    "timeout from altinn throws exception" {
+    "GatewayTimeout fra Altinn gir ServerResponseException" {
+        val altinnClient = mockAltinnClient(HttpStatusCode.GatewayTimeout)
+
         shouldThrowExactly<ServerResponseException> {
-            buildClient(HttpStatusCode.GatewayTimeout, "").hentOrgMedRettigheterForPerson(identitetsnummer)
+            altinnClient.hentRettighetOrganisasjoner(IDENTITETSNUMMER)
         }
     }
 
-    "timeout from altinn throws AltinnBrukteForLangTidException" {
+    "BadGateway fra Altinn gir ${AltinnBrukteForLangTidException::class.simpleName}" {
+        val altinnClient = mockAltinnClient(HttpStatusCode.BadGateway)
+
         shouldThrowExactly<AltinnBrukteForLangTidException> {
-            buildClient(HttpStatusCode.BadGateway, "").hentOrgMedRettigheterForPerson(identitetsnummer)
+            altinnClient.hentRettighetOrganisasjoner(IDENTITETSNUMMER)
         }
     }
 })
+
+private fun mockAltinnClient(status: HttpStatusCode, content: String = ""): AltinnClient {
+    val mockEngine = MockEngine {
+        respond(
+            content = content,
+            status = status,
+            headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+        )
+    }
+
+    val mockHttpClient = HttpClient(mockEngine) {
+        install(ContentNegotiation) {
+            jackson {
+                registerModule(JavaTimeModule())
+                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+            }
+        }
+    }
+
+    return AltinnClient(
+        "url",
+        "",
+        "",
+        "",
+        mockHttpClient
+    )
+}
+
+private fun String.readResource(): String =
+    ClassLoader.getSystemResource(this).readText()
