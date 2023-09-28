c9e948f7cca0991f1bd56e28162b814bd5a2994a
Mikael Bjerga <mikael.bjerga@nav.no>
2023-09-28 16:49:14 +0200

    Bump avhengigheter

--- src/main/kotlin/no/nav/helsearbeidsgiver/altinn/HttpUtils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/altinn/HttpUtils.kt
@@ -5,7 +5,7 @@ import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
+import no.nav.helsearbeidsgiver.utils.json.jsonConfig
 
 internal fun createHttpClient(): HttpClient =
     HttpClient(Apache) { customize() }
@@ -14,10 +14,6 @@ internal fun HttpClientConfig<*>.customize() {
     expectSuccess = true
 
     install(ContentNegotiation) {
-        json(
-            Json {
-                ignoreUnknownKeys = true
-            }
-        )
+        json(jsonConfig)
     }
 }
