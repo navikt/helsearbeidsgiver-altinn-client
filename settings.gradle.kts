3f828cf94ff4430b1c9595598809aa5c747604a6
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-10-02 10:28:44 +0200

    Fjern sonarqube (#10)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,11 @@ rootProject.name = "altinn-client"
 pluginManagement {
     val kotlinVersion: String by settings
     val kotlinterVersion: String by settings
-    val sonarqubeVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         id("org.jmailen.kotlinter") version kotlinterVersion
-        id("org.sonarqube") version sonarqubeVersion
         id("maven-publish")
     }
 }
