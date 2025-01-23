29d3ca37fcfbba87cce353427fefa21e056eaf71
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-01-23 10:06:09 +0100

    Logg info både til sikker og vanlig logg

--- src/main/kotlin/no/nav/helsearbeidsgiver/altinn/Altinn3OBOClient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/altinn/Altinn3OBOClient.kt
@@ -10,6 +10,7 @@ import kotlinx.serialization.Serializable
 import no.nav.helsearbeidsgiver.utils.cache.LocalCache
 import no.nav.helsearbeidsgiver.utils.cache.getIfCacheNotNull
 import no.nav.helsearbeidsgiver.utils.log.logger
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 
 /**
  * Klient som benytter Team Fager sitt API for å hente hvilke tilganger en innlogget bruker har i hvilke virksomheter/bedrifter.
@@ -22,6 +23,7 @@ class Altinn3OBOClient(
     cacheConfig: CacheConfig? = null,
 ) {
     private val logger = this.logger()
+    private val sikkerLogger = sikkerLogger()
 
     private val urlString = "$baseUrl/altinn-tilganger"
     private val httpClient = createHttpClient(maxRetries = 3)
@@ -44,7 +46,10 @@ class Altinn3OBOClient(
         getToken: () -> String,
     ): AltinnTilgangRespons =
         cache.getIfCacheNotNull(fnr) {
-            logger.debug("Henter Altinntilganger fra Fager sitt obo-endepunkt for ${fnr.take(5)}XXXXX")
+            "Henter Altinntilganger fra Fager sitt obo-endepunkt for ${fnr.take(5)}XXXXX".also {
+                logger.info(it)
+                sikkerLogger.info(it)
+            }
 
             httpClient
                 .post(urlString) {
@@ -52,8 +57,11 @@ class Altinn3OBOClient(
                     bearerAuth(getToken())
                     setBody(tilgangRequest)
                 }.body<AltinnTilgangRespons>()
-                .also {
-                    logger.debug("Hentet Altinntilganger for ${fnr.take(5)}XXXXX med ${it.hierarki.size} hovedenheter.")
+                .also { respons ->
+                    "Hentet Altinntilganger for ${fnr.take(5)}XXXXX med ${respons.hierarki.size} hovedenheter.".also {
+                        logger.info(it)
+                        sikkerLogger.info(it)
+                    }
                 }
         }
 
