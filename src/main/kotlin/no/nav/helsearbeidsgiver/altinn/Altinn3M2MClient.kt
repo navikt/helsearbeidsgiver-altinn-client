1436b00074b179995c772281f8694dbff8ef807a
Gustav Berggren <gustav.berggren@nav.no>
2026-02-06 13:53:21 +0100

    Slå opp altinn 2 og 3 rettigheter samtidig

--- src/main/kotlin/no/nav/helsearbeidsgiver/altinn/Altinn3M2MClient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/altinn/Altinn3M2MClient.kt
@@ -47,9 +47,22 @@ class Altinn3M2MClient(
                 }
         }
 
-    suspend fun hentTilganger(fnr: String): Set<String> = hentHierarkiMedTilganger(fnr).tilgangTilOrgNr["$serviceCode:1"].orEmpty()
+    suspend fun hentTilganger(fnr: String): Set<String> =
+        hentHierarkiMedTilganger(fnr).let {
+            val altinn2Tilganger =
+                it.organisasjonerMedAltinn2Tilgang()
+            val altinn3Tilganger = it.organisasjonerMedAltinn3Tilgang()
+            sikkerLogger().info(
+                "Hentet tilganger for ${fnr.take(6)}XXXX: antall tilganger Altinn2: ${altinn2Tilganger.size}, Altinn3: ${altinn3Tilganger.size}",
+            )
+            altinn2Tilganger + altinn3Tilganger
+        }
 
-    suspend fun hentAltinn3Tilganger(fnr: String): Set<String> = hentHierarkiMedTilganger(fnr).tilgangTilOrgNr[ressurs.value].orEmpty()
+    suspend fun hentAltinn3Tilganger(fnr: String): Set<String> = hentHierarkiMedTilganger(fnr).organisasjonerMedAltinn3Tilgang()
+
+    fun AltinnTilgangRespons.organisasjonerMedAltinn2Tilgang(): Set<String> = tilgangTilOrgNr["$serviceCode:1"].orEmpty()
+
+    fun AltinnTilgangRespons.organisasjonerMedAltinn3Tilgang(): Set<String> = tilgangTilOrgNr[ressurs.value].orEmpty()
 
     suspend fun harTilgangTilOrganisasjon(
         fnr: String,
